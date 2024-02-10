a224758f0f620107f4abdc207d99b29b7662d26c
tily <tidnlyam@gmail.com>
2024-02-10 12:33:46 +0900

    update app.py to get conversations replies in the thread

--- bottest/app.py
+++ bottest/app.py
@@ -1,6 +1,6 @@
+from datetime import datetime
 import logging
 import os
-import re
 import sys
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -9,13 +9,21 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 @app.event("app_mention")
-def app_mention(event, say, logger):
+def app_mention(event, client, say, logger):
     logger.info(event)
 
     to = event["user"]
     text = f"<@{to}> Hello"
 
     if "thread_ts" in event:
+        response = client.conversations_replies(
+            channel=event["channel"],
+            ts=event["thread_ts"],
+        )
+        for message in response["messages"]:
+            datetime_str = datetime.fromtimestamp(int(float(message["ts"])))
+            logger.info(f"{message['user']}: {message['text']} ({datetime_str})")
+
         say(text=text, thread_ts=event["thread_ts"])
     else:
         say(text=text, thread_ts=event["ts"])
